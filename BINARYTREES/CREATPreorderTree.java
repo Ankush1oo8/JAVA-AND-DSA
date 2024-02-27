be3c4a1b14e8179c0037d460de7c6c6c170a74da
Ankush1oo8 <ankushchudiwal@outlook.com>
2024-02-27 19:32:51 +0530

    findind root node in preorder

---
+++ BINARYTREES/CREATPreorderTree.java
@@ -0,0 +1,37 @@
+package BINARYTREES;
+
+public class CREATPreorderTree{
+    static class node{
+        int data;
+        node left;
+        node right;
+        
+
+        node(int data){
+           this.data=data;
+           this.left=null;
+           this.right=null;
+        }
+    }
+    static class BinaryTree{
+        static int idx=-1;
+        public static node buildTree(int nodes[]){
+            idx++;
+            if(nodes[idx]==-1){
+                return null;
+            }
+
+            node newNode=new node(nodes[idx]);
+            newNode.left=buildTree(nodes);
+            newNode.right=buildTree(nodes);
+            return newNode;
+        }
+    }
+    public static void main(String args[]) {
+        int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        BinaryTree tree=new BinaryTree();
+         node root=tree.buildTree(nodes);
+        System.out.println(root.data);
+    
+    }
+}
