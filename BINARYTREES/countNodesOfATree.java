7c0e4eccd50e0a619f6e0afd7bb410ba65a00331
Ankush1oo8 <ankushchudiwal@outlook.com>
2024-02-28 00:19:06 +0530

    counting nodes in the tree

---
+++ BINARYTREES/countNodesOfATree.java
@@ -0,0 +1,46 @@
+package BINARYTREES;
+
+public class countNodesOfATree {
+    static class node{
+        int data;
+        node left;
+        node right;
+        
+
+        node(int data){
+           this.data=data;
+           this.left=null;
+           this.right=null;
+        }
+    }
+    static class BinaryTree{
+        static int idx=-1;
+        public static node buildTree(int nodes[]){
+            idx++;
+            if(nodes[idx]==-1){
+                return null;
+            }
+
+            node newNode=new node(nodes[idx]);
+            newNode.left=buildTree(nodes);
+            newNode.right=buildTree(nodes);
+            return newNode;
+        }
+        public static int cout(node root){
+            if(root==null){
+                return 0;
+            }
+            int lc=cout(root.left);
+            int rc=cout(root.right);
+            int count=rc+lc+1;
+            return count;
+        }
+    }
+    public static void main(String args[]) {
+        int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        BinaryTree tree=new BinaryTree();
+         node root=tree.buildTree(nodes);
+        System.out.println(root.data);
+        System.out.println(tree.cout(root));
+    }
+}
