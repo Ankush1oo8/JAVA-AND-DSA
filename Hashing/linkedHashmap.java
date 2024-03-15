a99b7249ba7656c3f6564ff345404dc78129fdcd
Ankush1oo8 <ankushchudiwal@outlook.com>
2024-03-15 15:20:24 +0530

    Linked Hashmap

---
+++ Hashing/linkedHashmap.java
@@ -0,0 +1,13 @@
+package Hashing;
+
+import java.util.*;
+public class linkedHashmap {
+public static void main(String[] args) {
+    LinkedHashMap<String, Integer> h=new LinkedHashMap<>();
+    h.put("India", 100);
+    h.put("China",150);
+    h.put("USA", 50);
+    System.out.println(h);
+}
+    
+}
