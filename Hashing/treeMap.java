3ddd5cd6776175a0847ad641adc5c4f7fa0f1bcb
Ankush1oo8 <ankushchudiwal@outlook.com>
2024-03-15 15:26:21 +0530

    treemap

---
+++ Hashing/treeMap.java
@@ -0,0 +1,14 @@
+package Hashing;
+
+import java.util.*;
+
+public class treeMap {
+   public static void main(String[] args) {
+    TreeMap<String, Integer>tm=new TreeMap<>();
+    tm.put("India",100);
+    tm.put("China",150);
+    tm.put("Indoneshia",50);
+    System.out.println(tm);
+    //sorting based on key
+   }
+}
