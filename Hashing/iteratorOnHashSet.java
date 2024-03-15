83e6272f85bb3a3d842c1974b8c6f5ad29384db3
Ankush1oo8 <ankushchudiwal@outlook.com>
2024-03-16 04:58:14 +0530

    iterteration on HashSet

---
+++ Hashing/iteratorOnHashSet.java
@@ -0,0 +1,17 @@
+package Hashing;
+import java.util.*;
+public class iteratorOnHashSet {
+    public static void main(String[] args) {
+        HashSet<Integer>set=new HashSet<>();
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        set.add(12);
+        set.add(24);
+        set.add(36);
+        Iterator it=set.iterator();
+        while(it.hasNext()){
+            System.out.println(it.next());
+        }
+    }
+}
