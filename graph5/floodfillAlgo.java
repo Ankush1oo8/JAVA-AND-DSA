0586cb462a63a9729d8529613f0577946b9f6e34
Ankush1oo8 <ankushchudiwal@outlook.com>
2024-03-22 03:40:11 +0530

    flood fill algorithm

---
+++ graph5/floodfillAlgo.java
@@ -0,0 +1,24 @@
+package graph5;
+import java.util.*;
+public class floodfillAlgo {
+    public  void helper(int [][] image, int sr,int sc,int color,boolean vis[][],int orgcol){
+        if(sr<0 || sc<0 || sr>=image.length || sc>=image[0].length || vis[sr][sc] || image[sr][sc]!=orgcol){
+            return;
+        }
+        
+        //left
+        helper(image, sr, sc-1, color, vis, orgcol);
+        //right
+        helper(image, sr, sc+1, color, vis, orgcol);
+        //up
+        helper(image, sr-1, sc, color, vis, orgcol);
+        //down
+        helper(image, sr+1, sc, color, vis, orgcol);
+    }
+    public int[][] floodfill(int[][] image, int sr,int sc,int color){
+        boolean vis[][]=new boolean[image.length][image[0].length];
+        helper(image,sr,sc,color,vis,image[sr][sc]);
+        return image;
+    }
+   
+}
