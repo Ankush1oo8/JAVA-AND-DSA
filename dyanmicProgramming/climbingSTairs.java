b8aa081d5df025522339955dc304346d2ac76588
Ankush1oo8 <ankushchudiwal@outlook.com>
2024-03-23 02:26:57 +0530

    climbing stairs using Tabulation

--- dyanmicProgramming/climbingSTairs.java
+++ dyanmicProgramming/climbingSTairs.java
@@ -16,11 +16,24 @@ public class climbingSTairs {
         ways[n] = stairsClimb(n - 1, ways) + stairsClimb(n - 2, ways);
         return ways[n];
     }
+    public static int tabulation(int n){
+        int dp[]=new int[n+1];
+        dp[0]=1;
+        for(int i=1;i<=n;i++){
+            if(i==1){
+                dp[i]=dp[i-1];
+            }else{
+                dp[i]=dp[i-1]+dp[i-2];
+            }
+        }
+        return dp[n];
+    }
 
     public static void main(String[] args) {
         int n = 5;
         int ways[] = new int[n + 1];
         Arrays.fill(ways, -1);
         System.out.println(stairsClimb(n, ways));
+        System.out.println(tabulation(n));
     }
 }
