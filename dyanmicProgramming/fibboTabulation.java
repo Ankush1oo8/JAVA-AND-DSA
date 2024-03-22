f6b99ce33d32aed63e27e9c6671a8ea28d6a93ec
Ankush1oo8 <ankushchudiwal@outlook.com>
2024-03-23 01:34:05 +0530

    fibbonacci in tabulation way

--- dyanmicProgramming/fibboTabulation.java
+++ dyanmicProgramming/fibboTabulation.java
@@ -5,16 +5,17 @@ package dyanmicProgramming;
  */
 public class fibboTabulation {
 
-    public static int fibbo(int n){
-        int dp[]=new int[n+1];
-        dp[1]=1;
-        dp[0]=0;
+    public static int fibbo(int n) {
+        int dp[] = new int[n + 1];
+        dp[1] = 1;
+        dp[0] = 0;
 
-        for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
         return dp[n];
     }
+
     public static void main(String[] args) {
         System.out.println(fibbo(5));
     }
