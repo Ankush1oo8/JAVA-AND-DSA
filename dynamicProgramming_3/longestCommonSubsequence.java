a23d8779e90811d59e42ae1acd548db12e1b47a4
Ankush1oo8 <ankushchudiwal@outlook.com>
2024-03-25 16:18:55 +0530

    longest common subsequence using memoization

---
+++ dynamicProgramming_3/longestCommonSubsequence.java
@@ -0,0 +1,46 @@
+package dynamicProgramming_3;
+
+public class longestCommonSubsequence {
+    //recursion
+    public static int lcs(String str1,String str2,int n, int m){
+        if(n==0 || m==0){
+            return 0;
+        }
+        if(str1.charAt(n-1)==str2.charAt(m-1)){
+            return lcs(str1, str2, n-1, m-1)+1;
+        }else{
+            int ans1=lcs(str1, str2, n-1, m);
+            int ans2=lcs(str1, str2, n, m-1);
+            return Math.max(ans1, ans2);
+        }
+        
+    }
+    //memoization
+    public static int lcs2(String str1,String str2,int n, int m,int dp[][]){
+        if(n==0 || m==0){
+            return 0;
+        }
+        if(dp[n][m]!=-1){
+            return dp[n][m];
+        }
+        if(str1.charAt(n-1)==str2.charAt(m-1)){
+            return dp[n][m]=lcs2(str1, str2, n-1, m-1,dp)+1;
+        }else{
+            int ans1=lcs2(str1, str2, n-1, m,dp);
+            int ans2=lcs2(str1, str2, n, m-1,dp);
+            return dp[n][m]=Math.max(ans1, ans2);
+        }
+        
+    }
+    public static void main(String[] args) {
+        String str1="abcdge";
+        String str2="abedg";
+        int dp[][]=new int[str1.length()+1][str2.length()+1];
+        for(int i=0;i<str1.length()+1;i++){
+            for(int j=0;j<str2.length()+1;j++){
+                dp[i][j]=-1;
+            }
+        }
+        System.out.println(lcs2(str1, str2, str1.length(), str2.length(),dp) );
+    }
+}
